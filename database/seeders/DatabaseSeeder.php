122334f02637c4fd505e63d6c77acf3fe073e8d6
HoangBaoPhuc369 <hoangbaophuc369@gmail.com>
2022-04-12 12:53:30 +0700

    Done Section 3

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Disable foreign key constraints for users and enable it again.
+        Schema::disableForeignKeyConstraints();
+        \App\Models\User::truncate();
+        \App\Models\Role::truncate();
+        \App\Models\Category::truncate();
+        \App\Models\Post::truncate();
+        \App\Models\Tag::truncate();
+        \App\Models\Comment::truncate();
+        \App\Models\Image::truncate();
+
+        Schema::enableForeignKeyConstraints();
+
+        // Create roles and users
+        \App\Models\Role::factory(1)->create();
+        $users = \App\Models\User::factory(10)->create();
+
+        foreach ($users as $user) {
+            $user->image()->save(\App\Models\Image::factory()->make());
+        }
+
+        \App\Models\Category::factory(10)->create();
+
+        $posts = \App\Models\Post::factory(10)->create();
+        
+        \App\Models\Comment::factory(100)->create();
+        \App\Models\Tag::factory(10)->create();
+
+        foreach ($posts as $post) {
+            $tag_ids = [];
+
+            $tag_ids[] = \App\Models\Tag::all()->random()->id;
+            $tag_ids[] = \App\Models\Tag::all()->random()->id;
+            $tag_ids[] = \App\Models\Tag::all()->random()->id;
+
+            $post->tags()->sync($tag_ids );
+            $post->image()->save(\App\Models\Image::factory()->make());
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Image;
+
+Route::get('/createpost',function(){
+    $image = Image::find(1);
+    return $image->imageable;
+});
+
+
+
+
+
+
+
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+
+Route::get('/post', function () {
+    return view('post');
+})->name('post');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+require __DIR__.'/auth.php';
